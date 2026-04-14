103494115ee2ea0816d7d4f64e4b2c7ae9a20cf1
Rasul Khan <rasulkhan.nits@gmail.com>
2026-04-14 16:53:24 +0200

    fix: update babel and metro configurations for consistency and remove unused presets

--- mobile/metro.config.js
+++ mobile/metro.config.js
@@ -1,25 +1,6 @@
-// const path = require("path");
-// const { getDefaultConfig } = require("expo/metro-config");
-// const { withNativeWind } = require("nativewind/metro");
-
-// const projectRoot = __dirname;
-// const workspaceRoot = path.resolve(projectRoot, "..");
-
-// const config = getDefaultConfig(projectRoot);
-
-// config.watchFolders = [workspaceRoot];
-// config.resolver.nodeModulesPaths = [
-//   path.resolve(workspaceRoot, "node_modules"),
-// ];
-// config.resolver.disableHierarchicalLookup = true;
-
-// config.resolver.sourceExts.push("cjs");
-
-// module.exports = withNativeWind(config, { input: "./src/global.css" });
-
+const path = require("path");
 const { getDefaultConfig } = require("expo/metro-config");
-const { withNativeWind } = require("nativewind/metro");
 
 const config = getDefaultConfig(__dirname);
 
-module.exports = withNativeWind(config, { input: "./src/global.css" });
+module.exports = config;
